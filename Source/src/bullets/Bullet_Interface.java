37ad06ee8d092fee965f192897cf50ce10c3cab7
James <jtronlabs@gmail.com>
2015-07-31 14:47:04 -0700

    I started using git commit -a -m for a while. Appapparently that does not add everything that git add -A adds. This is a part of the last commit, just with all the files actually added

---
+++ Source/src/bullets/Bullet_Interface.java
@@ -0,0 +1,16 @@
+package bullets;
+
+import interfaces.Shooter;
+import android.content.Context;
+
+  
+
+public abstract class Bullet_Interface{
+	
+	public final static int BULLET_LEFT=20,
+			BULLET_MIDDLE=50,
+			BULLET_RIGHT=80;
+	
+	public abstract BulletView getBullet(Context context,Shooter shooter,float bulletSpeedY,int bulletDamage);
+	
+}
