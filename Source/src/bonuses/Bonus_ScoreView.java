c7a3aab2880dab7e99e23fa88b66f397a94084c6
James <jtronlabs@gmail.com>
2015-08-05 21:08:01 -0700

    scaled score bonuses with level

--- Source/src/bonuses/Bonus_ScoreView.java
+++ Source/src/bonuses/Bonus_ScoreView.java
@@ -1,10 +1,13 @@
 package bonuses;
 
+import levels.AttributesOfLevels;
 import helpers.MediaController;
 import interfaces.GameActivityInterface;
 import interfaces.MovingViewInterface;
+import android.content.SharedPreferences;
 import android.widget.RelativeLayout;
 
+import com.jtronlabs.space_shooter.GameActivity;
 import com.jtronlabs.space_shooter.R;
 
 public class Bonus_ScoreView extends BonusView implements MovingViewInterface{
@@ -18,6 +21,22 @@ public class Bonus_ScoreView extends BonusView implements MovingViewInterface{
 	public void applyBenefit(){
 		MediaController.playSoundEffect(getContext(), MediaController.SOUND_BONUS);
 		
-		((GameActivityInterface) this.getContext()).incrementScore(1000);
+		SharedPreferences gameState = getContext().getSharedPreferences(GameActivity.GAME_STATE_PREFS, 0);
+		final int level = gameState.getInt(GameActivity.STATE_LEVEL, 0);
+		
+		int amtToIncrementScore = 0;		
+		if(level < AttributesOfLevels.LEVELS_BEGINNER) {
+			amtToIncrementScore = 1000;
+		}else if(level < AttributesOfLevels.LEVELS_LOW) {
+			amtToIncrementScore = 1500;
+		}else if(level < AttributesOfLevels.LEVELS_MED){
+			amtToIncrementScore = 2000;			
+		}else if(level < AttributesOfLevels.LEVELS_HIGH){
+			amtToIncrementScore = 4000;			
+		}else{
+			amtToIncrementScore = 6000;			
+		}
+		
+		((GameActivityInterface) this.getContext()).incrementScore(amtToIncrementScore);
 	}
 }
